6541db2bff2ff7c62c73d4a0aa7f582d4485b2f0
Balage <balage112@gmail.com>
2022-09-07 21:03:34 +0200

    Added Room and Message model

--- chatterbox/admin.py
+++ chatterbox/admin.py
@@ -1,3 +1,6 @@
 from django.contrib import admin
-
+from chatterbox.models import Room
+from chatterbox.models import Message
 # Register your models here.
+admin.site.register(Room)
+admin.site.register(Message)
--- chatterbox/models.py
+++ chatterbox/models.py
@@ -1,3 +1,33 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 # Create your models here.
+
+
+class Room(models.Model):
+    name = models.CharField(max_length=200)
+    description = models.TextField(null=True, blank=True)
+    created = models.DateTimeField(auto_now_add=True)
+    updated = models.DateTimeField(auto_now=True)
+
+    class Meta:
+        ordering = ['-created', '-updated']
+
+    def __str__(self):
+        return self.name
+
+
+class Message(models.Model):
+    body = models.TextField() # null je defaultně False a blank také False
+    room = models.ForeignKey(Room, on_delete=models.CASCADE) # on_delete=models.CASCADE = když se smaže room tak se smažou i lavinově zprávy
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    created = models.DateTimeField(auto_now_add=True) # zazanamenej čas při vytvoření zprávy auto_now_add aniž bychom na created šáhli tak jakmile se vytvoří zpráva tak Django zazanmená aktuální čas
+                                                      # vytvoření zprávy
+    updated = models.DateTimeField(auto_now=True) # auto_now = při jakékoliv změněn zazanammenj čas
+
+    class Meta:
+        ordering = ['-created','-updated'] # bez minus je v defaultu ASC, s minusem nastavím DESC
+
+    def __str__(self):
+        return self.body[0:50] # zobrazí se mi prvních 50 znaků, abych to neměl tak dlouhý
+
